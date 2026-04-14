cb7ec387349e60a1c0e2f0c72fb41f547a03b8fa
Melina Amestoy <107568898+cybercounselor@users.noreply.github.com>
2026-04-14 14:22:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json({ limit: "10mb" }));
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://calistenia.australkungfu.com/");
   res.header("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   if (req.method === "OPTIONS") return res.sendStatus(200);
